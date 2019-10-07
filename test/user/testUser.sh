146d3645da703c8256851c7c5fef8bdf3c30b2b5
DA01171997 <duy.ado@csu.fullerton.edu>
2019-10-07 00:35:17 -0700

    delete music.db, cache, and moved curl script to test

--- test/user/testUser.sh
+++ test/user/testUser.sh
@@ -2,6 +2,6 @@
 curl --verbose \
 	--request POST \
 	--header 'Content-Type: application/json' \
-	--data @newuser.json \
+	--data @test/user/newuser.json \
 	http://127.0.0.1:5000/api/v1/user/register
 
